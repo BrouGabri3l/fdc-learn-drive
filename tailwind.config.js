70ef8c596138ca1d1f0c565d269bab6295bb5870
Gabriel Rodrigues <gabriel-rodriguesbs@hotmail.com>
2022-12-05 14:02:24 -0300

    adding colors theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,35 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary:{
+          "blue":"#2A9FBC",
+          black:{
+            "blue": "#005E83"
+          },
+          gray:{
+            dark:"#5C585B"
+          }
+        },
+        secondary:{
+          gray:{
+            "light":"#F8F8F8",
+            "medium": "#E4E2E2"
+          },
+          white:"#fff",
+        },
+        terciary:{
+          orange:"#FAA635",
+          yellow:"#F2CF52",
+          gray:{
+            "blue":"#B9CAD2"
+          },
+        },
+        "error":"#D32B2B",
+        "success":"#4ABA55"
+      }
+    },
   },
   plugins: [],
 }
