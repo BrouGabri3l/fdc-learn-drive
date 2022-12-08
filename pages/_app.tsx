e688cf0a41d895b31f42018af936a7a89647761c
Gabriel Rodrigues <gabriel-rodriguesbs@hotmail.com>
2022-12-08 16:29:54 -0300

    configuring ts and adding icons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,11 @@
 import '../styles/globals.css'
 import {Open_Sans} from '@next/font/google'
+import { AppProps } from 'next/app'
 const openSans = Open_Sans({
   subsets:['latin'],
   variable:'--font-openSans'
 })
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return( 
     <main className={openSans.className}>
       <Component {...pageProps} />
