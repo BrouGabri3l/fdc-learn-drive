b0a23c62aa1f98062d8139b788912047953e3a6a
Gabriel Rodrigues <gabriel-rodriguesbs@hotmail.com>
2022-12-08 16:34:11 -0300

    exporting icons

--- components/icons.js
+++ components/icons.js
@@ -3,6 +3,12 @@ import FrameIcon from '/public/Frame.svg'
 import DownloadIcon from '/public/Download.svg'
 import CloseIcon from '/public/Close.svg'
 import AcademyIcon from '/public/Academy.svg'
+import IdeaIcon from '/public/Idea.svg'
+import GraphicIcon from '/public/Graphic.svg'
+import HatIcon from '/public/Hat.svg'
+import PeopleIcon from '/public/People.svg'
+import QuoteIcon from '/public/quote.svg'
+import DownArrow from '/public/arrow.svg'
 export {
-    FrameIcon,PlusIcon,DownloadIcon,CloseIcon,AcademyIcon
+    FrameIcon,PlusIcon,DownloadIcon,CloseIcon,AcademyIcon,IdeaIcon,GraphicIcon,HatIcon,PeopleIcon,QuoteIcon,DownArrow
 }
