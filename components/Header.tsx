188ea29d2b99d5c18dd0ac3b6ff4fc74952ffc15
Gabriel Rodrigues <gabriel-rodriguesbs@hotmail.com>
2022-12-08 16:31:49 -0300

    defining Button and Header as tsx

---
+++ components/Header.tsx
@@ -0,0 +1,28 @@
+import Button from './Button'
+import Logo from '/public/Logo.svg'
+import Menu from '/public/menu.svg'
+import { FrameIcon, CloseIcon } from './icons'
+import { useState } from 'react'
+export default function Header() {
+    const [isActive, setIsActive] = useState(false)
+    const handleActive = () => {
+        setIsActive(!isActive)
+    }
+    return (
+        <header className="px-16 py-6 ">
+            <div className="max-w-7xl mx-auto flex justify-between align-center">
+                <Logo />
+                <nav className="flex gap-4 max-lg:hidden" >
+                    <Button theme="outline">Retomar</Button>
+                    <Button theme="primary" className="bg-terciary-orange" icon={FrameIcon}>Quero Começar Agora</Button>
+                </nav>
+                <button onClick={handleActive} className="w-8 h-8 lg:hidden" title='Abrir Menu'><Menu/></button>
+                <nav className={`${isActive ? "flex" : ' hidden'} fixed p-6 gap-3 right-0 top-0 w-full flex-col-reverse bg-white`}>
+                    <Button theme="outline">Retomar</Button>
+                    <Button theme="primary" className="bg-terciary-orange" icon={FrameIcon}>Quero Começar Agora</Button>
+                    <button onClick={handleActive} className="w-5 h-5 self-end" title="Fechar Menu"><CloseIcon  /></button>
+                </nav>
+            </div>
+        </header>
+    )
+}
